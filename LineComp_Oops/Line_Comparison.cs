6a0460bb5a0c49f18255db0b0853c191ac50c670
Mohanraj209 <mohanraj1715@gmail.com>
2022-03-02 01:42:50 +0530

    Program to calculate the length using Cartesian system

---
+++ LineComp_Oops/Line_Comparison.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LineComp_Oops
+{
+    internal class Line_Comparison
+    {
+		public static void Cal_Length()
+		{
+			int x1, x2, y1, y2;
+			double dis;
+			x1 = 2; y1 = 2; x2 = 5; y2 = 5;
+			dis = Math.Sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+			Console.WriteLine("\n Length of line of coordinates" + "(" + x1 + "," + y1 + ")," + "(" + x2 + "," + y2 + ") is equal to " + dis);
+		}
+	}
+}
