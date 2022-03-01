76c62c715ea5c51fecc0f38cde67f686fe4eeda2
Mohanraj209 <mohanraj1715@gmail.com>
2022-03-02 01:39:08 +0530

    Displaying Message

---
+++ LineComp_Oops/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LineComp_Oops
+{
+    internal class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("----------------**** Geometry Line Comparison App ****----------------");
+            
+        }
+    }
+}
